c14b7e54f548f18bdcf5dfc87cde01d99538bcbc
ivddorrka <ivddorrka@gmail.com>
2020-12-12 18:47:00 +0200

    Added doc-on

--- predskazaniya/pred.py
+++ predskazaniya/pred.py
@@ -1,6 +1,12 @@
+'''
+Your fortuneteller
+'''
 import random
 
 def read_file(file):
+    '''
+    Opens file and returns a list of 365 quotes
+    '''
     file_open = open(file, "r")
     file_lst = file_open.readlines()
     file_list = ''.join(map(str, file_lst)).split('\n')
@@ -9,6 +15,9 @@ def read_file(file):
 # print(read_file('monday'))
 
 def random_quote(file):
+    '''
+    Returns 1 random quote from read_file() func
+    '''
     list_quotes = read_file(file)
     return random.choice(list_quotes)
-print(random_quote('monday'))
+# print(random_quote('monday'))
